48ab565dc3a1544723650cdaf73c1722a2b09071
TrevorMcCormack <147773578+TrevorMcCormack@users.noreply.github.com>
2023-11-06 18:48:01 -0500

    sharedMemBuff.hpp

--- CodingProject1/sharedMemBuff.hpp
+++ CodingProject1/sharedMemBuff.hpp
@@ -1,1 +1,22 @@
+#ifndef SHAREDMEMBUFF_HPP
+#define SHAREDMEMBUFF_HPP
 
+#include <semaphore.h>
+#include <stdlib.h>
+
+#define SIZE = 2
+
+
+struct sharedMemBuff {
+    int table[SIZE];
+
+    sem_t sem1;
+    sem_t empty;
+    sem_t full;
+
+    int in;
+    int out;
+};
+
+
+#endif
