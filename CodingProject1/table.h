c689523311a6b43d9de3f5475d8f74e9c636bb8e
TrevorMcCormack <147773578+TrevorMcCormack@users.noreply.github.com>
2023-11-07 15:44:04 -0500

    table struct

--- CodingProject1/table.h
+++ CodingProject1/table.h
@@ -1,1 +1,19 @@
+// Code for table structure
+// Trevor McCormack
+// CS33211
 
+#ifndef TABLE_H
+#define TABLE_H
+
+#include <semaphore.h>
+
+const int MAXITEMS = 10;            // max amount of items
+const int TABLESIZE = 2;            // max amount allowed in the table at once
+
+struct table {
+    int sharedBuffer[TABLESIZE];    // shared array for both programs to use
+    sem_t empty;                    // number of empty spaces
+    sem_t full;                     // number of full spaces
+};
+
+#endif
