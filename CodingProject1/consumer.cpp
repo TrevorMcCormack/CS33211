c162845b1b6f1cd0d5706770663d4462b5dfa049
TrevorMcCormack <147773578+TrevorMcCormack@users.noreply.github.com>
2023-11-06 19:19:01 -0500

    consumer.cpp

--- CodingProject1/consumer.cpp
+++ CodingProject1/consumer.cpp
@@ -9,3 +9,36 @@
 
 #include "../include/sharedMemBuff.hpp"
 
+int main(int argc, char *argv[]) {
+    int fd;
+    char *sharedMemPath;
+    struct sharedMemBuff consumerMem;
+
+    sharedMemPath = argv[1];                                                   // grab shared memory path from command
+
+    fd = shm_open(sharedMemPath, O_RDWR, 0);                                   // open shared memory
+    ftruncate(fd, sizeof(*consumerMem));                                       // set to the size of the struct 
+
+    consumerMem = static_cast<sharedMemBuff*>(mmap(NULL, sizeof(consumerMem), PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0));
+
+    for(int i = 4; i >= 0; --i) {
+        sem_wait(&consumerMem -> full);                                        // check if buffer is full
+        sem_wait(&consumerMem -> sem1);                                        // check if sem1 is not in use
+
+        int consumerValue;                                    
+        consumerValue = consumerMem -> table[consumerMem -> out];
+
+        std::cout << "Consumed: " << consumerValue << std::endl;
+
+        consumerValue -> out = ((consumerMem -> out) + 1) % SIZE;
+
+        sem_post(&consumerMem -> sem1);                                        // release sem1 from being active
+        sem_post(&consumerMem -> empty);                                       // mark buffer as empty
+
+        sleep(rand() % 10);
+    }
+
+    shm_unlink(sharedMemPath);
+
+    return 0;
+}
