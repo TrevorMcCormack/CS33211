c51fa0e06da8f3d479df548958f0e4e9469abd59
TrevorMcCormack <147773578+TrevorMcCormack@users.noreply.github.com>
2023-11-06 19:10:21 -0500

    tweaks

--- CodingProject1/producer.cpp
+++ CodingProject1/producer.cpp
@@ -13,34 +13,34 @@
 int main(int argc, char *argv[]) {
     int fd;
     char *sharedMemPath;
-    struct sharedMemBuff *shmp;
+    struct sharedMemBuff *producerMem;
 
     sharedMemPath = argv[1];                                            // grab shared memory path from command
 
     fd = shm_open(sharedMemPath, O_CREAT | O_EXCL | O_RDWR, 0600);      // open shared memory
-    ftruncate(fd, sizeof(*shmp));                                       // set to the size of the struct 
+    ftruncate(fd, sizeof(*producerMem));                                       // set to the size of the struct 
 
-    sem_init((&shmp -> sem1), 1, 1);                                    // initialize first semaphore
-    sem_init((*shmp -> empty), 1, SIZE);                                // initialize empty semaphore
-    sem_init((*shmp -> full), 1, 0);                                    // initialize full semaphore
+    sem_init((&producerMem -> sem1), 1, 1);                                    // initialize first semaphore
+    sem_init((&producerMem -> empty), 1, SIZE);                                // initialize empty semaphore
+    sem_init((&producerMem -> full), 1, 0);                                    // initialize full semaphore
 
-    shmp -> in = 0;
-    shmp -> out = 0;
+    producerMem -> in = 0;
+    producerMem -> out = 0;
 
     for(int i = 4; i >= 0; --i) {
-        sem_wait(*shmp -> empty);                                       // check if buffer is empty
-        sem_wait(*shmp -> sem1);                                        // check if sem1 is not in use
+        sem_wait(&producerMem -> empty);                                       // check if buffer is empty
+        sem_wait(&producerMem -> sem1);                                        // check if sem1 is not in use
 
         int producerValue = rand();                                     // create random producer value
 
         std::cout << "Produced: " << producerValue << std::endl;
 
-        shmp -> table[shmp -> in] = producerValue;
+        producerMem -> table[producerMem -> in] = producerValue;
 
-        shmp -> in = ((shmp -> in) + 1) % SIZE;
+        producerMem -> in = ((producerMem -> in) + 1) % SIZE;
 
-        sem_post(&shmp -> sem1);                                        // release sem1 from being active
-        sem_post(*shmp -> full);                                        // mark buffer as full
+        sem_post(&producerMem -> sem1);                                        // release sem1 from being active
+        sem_post(&producerMem -> full);                                        // mark buffer as full
 
         sleep(rand() % 10);
     }
