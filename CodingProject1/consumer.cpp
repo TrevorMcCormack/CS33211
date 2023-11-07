af677c935071ca94c9ba70eccf30b6fde1af23d9
TrevorMcCormack <147773578+TrevorMcCormack@users.noreply.github.com>
2023-11-06 19:07:16 -0500

    setup

--- CodingProject1/consumer.cpp
+++ CodingProject1/consumer.cpp
@@ -1,1 +1,11 @@
+//Code for consumer program
+//Trevor McCormack
+//CS33211
+
+#include <pthread.h>
+#include <semaphore.h>
+#include <iostream>
+#include <sys/mman.h>
+
+#include "../include/sharedMemBuff.hpp"
 
