74b5e2a701e3f2f896e8d18acec03b6f35781633
TrevorMcCormack <147773578+TrevorMcCormack@users.noreply.github.com>
2023-11-30 13:42:34 -0500

    m

--- CodingProject2/bankersalgorithm.cpp
+++ CodingProject2/bankersalgorithm.cpp
@@ -9,8 +9,10 @@
 #include <vector>
 
 
+// function to read data from the matrix 
 void inputFile(std::vector<std::vector<int>> &allocation, std::vector<std::vector<int>> &max, std::vector<int> &available, int &processes, int &resources) {
-
+    std::ifstream in;
+    in.open("matrix.csv");
 }
 
 
