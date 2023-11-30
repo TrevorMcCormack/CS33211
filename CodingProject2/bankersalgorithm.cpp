81b6e3b09ddc10989267daf5209bbebe2d28525b
TrevorMcCormack <147773578+TrevorMcCormack@users.noreply.github.com>
2023-11-30 13:19:51 -0500

    .cpp setup

--- CodingProject2/bankersalgorithm.cpp
+++ CodingProject2/bankersalgorithm.cpp
@@ -1,1 +1,20 @@
+// Code for Banker's Algorithm
+//Trevor McCormack
+// CS33211
 
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <ctype.h>
+#include <vector>
+
+
+void inputFile(std::vector<std::vector<int>> &allocation, std::vector<std::vector<int>> &max, std::vector<int> &available, int &pro, int &res) {
+
+}
+
+
+int main() {
+    
+    return 0;
+}
